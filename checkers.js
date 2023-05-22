19538e225eb562747a51b364317092cd8a8374c9
DaPoofDoof <126741360+DaPoofDoof@users.noreply.github.com>
2023-05-22 10:05:36 -0400

    work for 5/22/23

--- checkers.js
+++ checkers.js
@@ -1,10 +1,60 @@
-hanburgra = document.getElementsByClassName("checker")
+let checkerSpot = document.getElementsByClassName("checker")
+let gloudiao = document.getElementById("clicksound")
+let checkerPiece;
 
-function checkler() {
-    console.log(event.target)
+function placeCheckers() {
+
+    for(i=0; i <= 12; i++){
+
+
+        if (i % 2 == 0) {
+            h += 1
+        } else { 
+            return h
+        }
+
+       
+        checkerPiece = document.createElement('p');
+        checkerPiece.appendChild(row1[h])
+
+        checkerPiece.classList.add ="P1Checkers"
+        
+        
+    }
+
+    for(i=21; i <= 32; i++){
+
+        if (i % 2 == 0) {
+            h += 1
+        } else { 
+            return h
+        }
+        
+        checkerPiece = document.createElement('p');
+        checkerPiece.appendChild(row1[h])
+
+        checkerPiece.classList.add ="P1Checkers"
+        
+    }
 }
 
 
-for(i=0; i < hanburgra.length; i++) {
-    hanburgra[i].addEventListener("click", checkler)
+function checkler() {
+    console.log(event.target)
+    new Audio('click.wav').play()
+
+
+
+    // if (event.target == "P1Checkers") {
+
+
+
+    // }
+
+
+}
+
+
+for(i=0; i < checkerSpot.length; i++) {
+    checkerSpot[i].addEventListener("click", checkler)
 }
