e1e6f335d392de4541a7904a5ad9f04b2f6d7a48
DaPoofDoof <126741360+DaPoofDoof@users.noreply.github.com>
2023-05-24 08:29:12 -0400

    work for 5/24/23

--- checkers.js
+++ checkers.js
@@ -2,58 +2,68 @@ let checkerSpot = document.getElementsByClassName("checker");
 let gloudiao = document.getElementById("clicksound");
 let checkerPiece;
 let red = document.getElementById("red");
+let green = document.getElementById("green");
+let blue = document.getElementById("blue");
+let yellow = document.getElementById("yellow");
+let pink = document.getElementById("pink");
+let purple = document.getElementById("purple");
+let maroon = document.getElementById("maroon");
 let P1Checkers = document.getElementsByClassName("P1Checkers");
-
-function placeCheckers() {
-    for (let i = 0; i <= 12; i++) {
-        checkerPiece = document.createElement('p');
-
-
-        checkerPiece.textContent = i; // Update with your desired content
-
-
-        checkerPiece.classList.add("P1Checkers");
-
-
-        P1Checkers.appendChild(checkerPiece);
-    }
-
-
-
-
-
-
-
-    for (let i = 21; i <= 32; i++) {
-
-        checkerPiece = document.createElement('p');
-
-        checkerPiece.textContent = i; // Update with your desired content
-
-        checkerPiece.classList.add("P1Checkers");
-
-        P1Checkers.appendChild(checkerPiece);
-
-    }
-}
+let turnBox = document.getElementById("colorChangingBox");
 
 function checkler() {
+
     console.log(this.id);
 
     new Audio('click.wav').play();
+
 }
 
+
+
+
+function placeCheckers() {
+
+    for (i = 1; i <= 12; i++) {
+
+        checkerPiece = document.createElement('p');
+
+        checkerPiece.classList.add("P1Checkers");
+
+        document.getElementById(i).appendChild(checkerPiece);
+    }
+
+    for (x = 21; x <= 32; x++) {
+
+        checkerPiece = document.createElement('p');
+
+        checkerPiece.classList.add("P2Checkers");
+
+        document.getElementById(x).appendChild(checkerPiece);
+    }
+}
+
+
+
 function toggleDropdown() {
+
     var dropdownContent = document.getElementById("player1-color");
+
     dropdownContent.classList.toggle("show");
 }
 
 window.onclick = function (event) {
+
     if (!event.target.matches(".dropdown-button")) {
+
         var dropdowns = document.getElementsByClassName("dropdown-content");
+
         for (var i = 0; i < dropdowns.length; i++) {
+
             var openDropdown = dropdowns[i];
+
             if (openDropdown.classList.contains("show")) {
+
                 openDropdown.classList.remove("show");
             }
         }
@@ -61,15 +71,37 @@ window.onclick = function (event) {
 };
 
 for (let i = 0; i < checkerSpot.length; i++) {
+
     checkerSpot[i].addEventListener("click", checkler);
 }
 
 function colorChanging() {
- let color = this.id;
-    
 
-     console.log(color);
-    P1Checkers[0].style.backgroundColor = color;
+    let color = this.id;
+
+    console.log(color);
+
+    turnBox.style.backgroundColor = color;
+    
+    // P1Checkers[0].style.backgroundColor = color;
+}
+
+function colorChanging2() {
+
+    let color = this.id;
+
+    console.log(color);
+
+    document.getElementById('colorTitle').style.backgroundColor = color;
+    
+    // P1Checkers[0].style.backgroundColor = color;
 }
 
 red.addEventListener('click', colorChanging);
+blue.addEventListener('click', colorChanging);
+green.addEventListener('click', colorChanging);
+yellow.addEventListener('click', colorChanging);
+pink.addEventListener('click', colorChanging);
+purple.addEventListener('click', colorChanging);
+maroon.addEventListener('click', colorChanging);
+placeCheckers();
