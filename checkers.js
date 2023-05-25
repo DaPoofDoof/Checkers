785292f1f966679a8fba05e7b6f1e0131462ca69
DaPoofDoof <126741360+DaPoofDoof@users.noreply.github.com>
2023-05-25 10:13:18 -0400

    work for 5/25/23

--- checkers.js
+++ checkers.js
@@ -1,35 +1,61 @@
 let checkerSpot = document.getElementsByClassName("checker");
 let gloudiao = document.getElementById("clicksound");
 let checkerPiece;
-let P1red = document.getElementById("red");
-let P1green = document.getElementById("green");
-let P1blue = document.getElementById("blue");
-let P1yellow = document.getElementById("yellow");
-let P1pink = document.getElementById("pink");
-let P1purple = document.getElementById("purple");
-let P1maroon = document.getElementById("maroon");
-let P2red = document.getElementById("red");
-let P2green = document.getElementById("green");
-let P2blue = document.getElementById("blue");
-let P2yellow = document.getElementById("yellow");
-let P2pink = document.getElementById("pink");
-let P2purple = document.getElementById("purple");
-let P2maroon = document.getElementById("maroon");
+let P1red = document.getElementById("P1red");
+let P1green = document.getElementById("P1green");
+let P1blue = document.getElementById("P1blue");
+let P1yellow = document.getElementById("P1yellow");
+let P1pink = document.getElementById("P1pink");
+let P1purple = document.getElementById("P1purple");
+let P1maroon = document.getElementById("P1maroon");
+let P2red = document.getElementById("P2red");
+let P2green = document.getElementById("P2green");
+let P2blue = document.getElementById("P2blue");
+let P2yellow = document.getElementById("P2yellow");
+let P2pink = document.getElementById("P2pink");
+let P2purple = document.getElementById("P2purple");
+let P2maroon = document.getElementById("P2maroon");
 let P1Checkers = document.getElementsByClassName("P1Checkers");
 let P2Checkers = document.getElementsByClassName("P2Checkers");
 let turnBox = document.getElementById("colorChangingBox");
+let checkerNum;
+let selectP1Color = document.getElementById("selectP1Color")
+let selectP2Color = document.getElementById("selectP2Color")
+let boxID = document.getElementsByClassName("checker");
+let availableSpot1;
+let availableSpot2;
+let get
+
 
 function checkler() {
-
-    console.log(this.id);
 
     new Audio('click.wav').play();
 
 }
 
 
-function movePiece(test){
-    console.log(test);
+function movePiece(){
+    checkerNum = event.target.id;
+    let parent1 = event.target.parentElement;
+    parent1 = parent1.parentElement;
+    parent1 = parent1.id;
+    parent1 = parent1.substring(3);
+    if(parent1 % 2 == 0){
+        availableSpot1 = document.createElement('p');
+        availableSpot1.classList.add("availableSpots");
+        availableSpot1.setAttribute("id", parseInt(checkerNum) + 3);
+        document.getElementById(parseInt(checkerNum) + 3).appendChild(availableSpot1);
+    }
+
+
+
+
+    console.log(checkerNum);
+    console.log(parent1)
+
+    // checkerNum = event.target.parentElement.id;
+    // console.log(checkerNum)
+    
 }
 
 
@@ -40,6 +66,8 @@ function placeCheckers() {
         checkerPiece = document.createElement('p');
 
         checkerPiece.classList.add("P1Checkers");
+        checkerPiece.setAttribute("id", i);
+        console.log(checkerPiece);
 
         document.getElementById(i).appendChild(checkerPiece);
     }
@@ -49,6 +77,7 @@ function placeCheckers() {
         checkerPiece = document.createElement('p');
 
         checkerPiece.classList.add("P2Checkers");
+        checkerPiece.setAttribute("id", x);
 
         document.getElementById(x).appendChild(checkerPiece);
     }
@@ -58,15 +87,15 @@ function placeCheckers() {
 
 
 
-
 function colorChanging() {
 
     let colorName = this.id;
-    let color = colorName.substring(1)
-    let color2 = P2Checkers[0].style.backgroundColor
+    let color = colorName.substring(2)
     console.log(color);
+    let color2 = P2Checkers[0].style.backgroundColor
+    console.log(color2);
 
-    if (!color == color2){
+    if (color != color2){
     for (i=0; i < 12; i++)
     P1Checkers[i].style.backgroundColor = color;
     }
@@ -75,14 +104,22 @@ function colorChanging() {
 
 function colorChanging2() {
 
-    let color = this.id;
-
+    let colorName = this.id;
+    let color = colorName.substring(2)
     console.log(color);
+    let color2 = P1Checkers[0].style.backgroundColor
+    console.log(color2);
 
-    P1Checkers[0].style.backgroundColor = color;
+    if (color != color2){
+        for (x=0; x < 12; x++)
+        P2Checkers[x].style.backgroundColor = color;
+        }
     
-    // P1Checkers[0].style.backgroundColor = color;
 }
+
+
+
+
 
 P1red.addEventListener('click', colorChanging);
 P1blue.addEventListener('click', colorChanging);
@@ -91,18 +128,26 @@ P1yellow.addEventListener('click', colorChanging);
 P1pink.addEventListener('click', colorChanging);
 P1purple.addEventListener('click', colorChanging);
 P1maroon.addEventListener('click', colorChanging);
-P2red.addEventListener('click', colorChanging);
-P2blue.addEventListener('click', colorChanging);
-P2green.addEventListener('click', colorChanging);
-P2yellow.addEventListener('click', colorChanging);
-P2pink.addEventListener('click', colorChanging);
-P2purple.addEventListener('click', colorChanging);
-P2maroon.addEventListener('click', colorChanging);
+P2red.addEventListener('click', colorChanging2);
+P2blue.addEventListener('click', colorChanging2);
+P2green.addEventListener('click', colorChanging2);
+P2yellow.addEventListener('click', colorChanging2);
+P2pink.addEventListener('click', colorChanging2);
+P2purple.addEventListener('click', colorChanging2);
+P2maroon.addEventListener('click', colorChanging2);
+
+
+
 placeCheckers();
 
+eventListeners();
 
 function eventListeners(){
-    for(p=1; P1Checkers.length >p; p++){
-        P1Checkers[p].addEventListener('click', movePiece(p))
+    for(p=0; P1Checkers.length >p; p++){
+        P1Checkers[p].addEventListener('click', movePiece);
+        // checkerSpot[p].addEventListener('click', movePiece)
     }
 }
+
+
+console.log(P2Checkers[0])
